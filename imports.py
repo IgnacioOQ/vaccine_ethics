ecc599b4e0a2087659ffa36055ebb37eb182fd42
Ignacio <ignacioojea@gmail.com>
2025-03-21 11:30:17 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -5,3 +5,19 @@ import matplotlib.colors as mcolors
 import time
 from IPython.display import clear_output
 import tqdm as tqdm
+
+# State mapping for grid visualization with colors
+state_mapping = {
+    'S': 1,  # Susceptible - green
+    'I': 2,  # Infected - red
+    'R': 3,  # Recovered - blue
+    'D': 4   # Dead - black
+}
+
+# Mapping the numerical state to colors for plotting
+color_mapping = {
+    1: 'green',  # Susceptible agents will be shown in green
+    2: 'red',    # Infected agents will be shown in red
+    3: 'blue',   # Recovered agents will be shown in blue
+    4: 'yellow'   # Dead agents will be shown in black
+}
